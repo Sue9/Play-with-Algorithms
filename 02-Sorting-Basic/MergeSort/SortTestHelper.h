da1a456349ffb7d30d63a9a883a29c088ee2aa16
Sue <ssica_789@163.com>
2018-11-21 18:23:16 +0800

    3-1, 3-2

---
+++ 02-Sorting-Basic/MergeSort/SortTestHelper.h
@@ -0,0 +1,109 @@
+/*
+ * SortTestHelper.h
+ *
+ *  Created on: 2018年11月19日
+ *      Author: Sue
+ */
+
+#ifndef SORTTESTHELPER_H_
+#define SORTTESTHELPER_H_
+
+#include <iostream>
+#include <ctime>
+#include <cassert>
+
+using namespace std;
+
+namespace SortTestHelper{
+
+    template<typename T>
+    void selectionSort( T arr[], int n ){
+        //find min in [i, n)
+        for(int i = 0; i < n; i++ ){
+            int minIndex = i;
+            for(int j = i; j < n; j++){
+                if (arr[j] < arr[minIndex])
+                    minIndex = j;
+            }
+
+            std::swap(arr[i], arr[minIndex]);
+
+        }
+
+    }
+
+
+	//generate random n elements, all in [rangeL, rangeR]
+	int *generateRandomArray(int n, int rangeL, int rangeR){
+		assert(rangeL <= rangeR);
+		int *arr = new int[n];
+		srand(time(NULL));
+
+		for(int i = 0; i < n; i++){
+			arr[i] = rand() % (rangeR - rangeL + 1) + rangeL;
+		}
+
+		return arr;
+	}
+
+	int *generateNearlyOrderedArray(int n, int swapTimes){
+	    int *arr = new int[n];
+	    for(int i = 0; i < n; i++){
+	        arr[n] = i;
+	    }
+
+	    srand(time(NULL));
+	    for(int i = 0; i < swapTimes; i++){
+	        int posX = rand() % n;
+	        int posY = rand() % n;
+	        swap(arr[posX], arr[posY]);
+	    }
+
+	    return arr;
+	}
+
+
+
+	template<typename T>
+	void printArray(T arr[], int n){
+		for(int i = 0; i < n; i++){
+			cout << arr[i] << " ";
+		}
+		cout << endl;
+		return;
+	}
+
+    template<typename T>
+    bool isSorted(T arr[], int n){
+	    for(int i = 0; i < n-1; i++){
+	        if(arr[i] > arr[i + 1])
+	            return false;
+	    }
+	    return true;
+	}
+
+
+	template<typename T>
+	void testSort( string sortName, void(*sort)(T[], int), T arr[], int n){
+		clock_t startTime = clock();
+		sort(arr, n);
+		clock_t endTime = clock();
+
+		assert( isSorted(arr, n));
+		cout << sortName << " : " << double(endTime - startTime) / CLOCKS_PER_SEC << " s" << endl;
+
+		return;
+	}
+
+	int* copyIntArray(int a[], int n){
+		int* arr = new int[n];
+		copy(a, a+n, arr);
+		return arr;
+	}
+
+
+}
+
+
+
+#endif /* SORTTESTHELPER_H_ */
