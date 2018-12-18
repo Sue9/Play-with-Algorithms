5db5980dc3f7a200c4a2201f26dea631dbf01b7d
Sue <ssica_789@163.com>
2018-12-18 18:36:08 +0800

    Quick sort complete

--- 02-Sorting-Basic/QuickSort/QuickSort.h
+++ 02-Sorting-Basic/QuickSort/QuickSort.h
@@ -2,9 +2,125 @@
 // Created by zhaoxu on 2018/11/28.
 //
 #include <iostream>
+#include "InsertionSort.h"
 
 #ifndef QUICKSORT_QUICKSORT_H
 #define QUICKSORT_QUICKSORT_H
+
+
+// //////////////////////////////////////////////////////////////////////////////
+//  Quick Sort Three Ways
+// //////////////////////////////////////////////////////////////////////////////
+
+
+
+template <typename T>
+void __quickSort3Ways(T arr[], int l, int r){
+    if(l - r <= 15){
+        insertionSort(arr, l, r);
+        return;
+    }
+
+
+    //partition
+    swap(arr[l], arr[rand()%(r-l+1)+l]);
+    T v = arr[l];
+    int lt = l;     //arr[l+1...lt] < v
+    int gt = r+1;   //arr[gt... r]  > v
+    int i = l + 1;  //arr[lt+1...i) ==v
+
+    while(i < gt){
+        if(arr[i] < v){
+            swap(arr[i], arr[lt+1]);
+            lt ++;
+            i ++;
+        }
+        else if(arr[i] > v){
+            swap(arr[i], arr[gt-1]);
+            gt --;
+        }
+        else{ // arr[i] == v
+            i++;
+        }
+
+    }
+
+    swap(arr[l], arr[lt]);
+
+
+    __quickSort3Ways(arr, l, lt-1);
+    __quickSort3Ways(arr, gt, r);
+
+}
+
+
+template <typename T>
+void quickSort3Ways(T arr[], int n){
+    srand(time(NULL));
+    __quickSort3Ways(arr, 0, n-1);
+}
+
+
+
+// //////////////////////////////////////////////////////////////////////////////
+// Quick Sort Two Pointers
+// //////////////////////////////////////////////////////////////////////////////
+
+template <typename T>
+int __partition2(T arr[], int l, int r){
+    swap(arr[l], arr[rand()%(r-l+1)+l]);
+
+    T v = arr[l];
+
+    // arr[l+1...j) <= v ; arr(j...r] >= v
+    int i = l+1 ,j = r;
+
+    while(true){
+        while(i <= r && arr[i] < v) i++;
+        while(j >= l+1 && arr[j] > v) j--;
+
+        if(i > j) break;
+
+        swap(arr[i], arr[j]);
+        i ++;
+        j --;
+    }
+
+    swap(arr[l], arr[j]);
+
+    return j;
+}
+
+template <typename T>
+void __quickSort2(T arr[], int l, int r){
+    if(r - l <= 15)
+    {
+        insertionSort(arr, l, r);
+        return;
+    }
+
+    int p2 = __partition2(arr, l, r);
+    __quickSort2(arr, l, p2-1);
+    __quickSort2(arr, p2+1, r);
+
+    return;
+}
+
+template <typename T>
+void quickSort2(T arr[], int n){
+    srand(time(NULL));
+    __quickSort2(arr, 0, n-1);
+
+}
+
+
+
+
+// //////////////////////////////////////////////////////////////////////////////
+// Quick sort basic idea
+// //////////////////////////////////////////////////////////////////////////////
+
+
 
 //do partition on arr[l...r]
 //return index p, so that arr[l...p-1] < arr[p] ; arr[p+1...r] > arr[p]
--- 02-Sorting-Basic/QuickSort/main.cpp
+++ 02-Sorting-Basic/QuickSort/main.cpp
@@ -5,52 +5,6 @@
 
 using namespace std;
 
-template <typename T>
-int __partition2(T arr[], int l, int r){
-    swap(arr[l], arr[rand()%(r-l+1)+l]);
-
-    T v = arr[l];
-
-    // arr[l+1...j) <= v ; arr(j...r] >= v
-    int i = l+1 ,j = r;
-
-    while(true){
-        while(i <= r && arr[i] < v) i++;
-        while(j >= l+1 && arr[j] > v) j--;
-
-        if(i > j) break;
-
-        swap(arr[i], arr[j]);
-        i ++;
-        j --;
-    }
-
-    swap(arr[l], arr[j]);
-
-    return j;
-}
-
-template <typename T>
-void __quickSort2(T arr[], int l, int r){
-    if(r - l <= 15)
-    {
-        insertionSort(arr, l, r);
-        return;
-    }
-
-    int p2 = __partition2(arr, l, r);
-    __quickSort2(arr, l, p2-1);
-    __quickSort2(arr, p2+1, r);
-
-    return;
-}
-
-template <typename T>
-void quickSort2(T arr[], int n){
-    srand(time(NULL));
-    __quickSort2(arr, 0, n-1);
-
-}
 
 
 int main() {
@@ -60,46 +14,57 @@ int main() {
     int* arr1 = SortTestHelper::generateRandomArray(n, 0, n);
     int* arr2 = SortTestHelper::copyIntArray(arr1, n);
     int* arr3 = SortTestHelper::copyIntArray(arr1, n);
+    int* arr4 = SortTestHelper::copyIntArray(arr1, n);
 
     SortTestHelper::testSort("Merge Sort", mergeSort, arr1, n);
-    SortTestHelper::testSort("Quick Sort", quickSort, arr2, n);
+//    SortTestHelper::testSort("Quick Sort", quickSort, arr2, n);
     SortTestHelper::testSort("Quick Sort 2 pointers", quickSort2, arr3, n);
+    SortTestHelper::testSort("Quick Sort 3 Ways", quickSort3Ways, arr4, n);
 
     delete[] arr1;
     delete[] arr2;
     delete[] arr3;
+    delete[] arr4;
 
     cout << endl;
 
+    //nearly ordered array
     int swapTimes = 100;
     cout<<"Test for Nearly Ordered Array, size = " << n <<" , swap time = " << swapTimes << endl;
     arr1 = SortTestHelper::generateNearlyOrderedArray(n, swapTimes);
     arr2 = SortTestHelper::copyIntArray(arr1, n);
     arr3 = SortTestHelper::copyIntArray(arr1, n);
+    arr4 = SortTestHelper::copyIntArray(arr1, n);
+
 
     SortTestHelper::testSort("Merge Sort", mergeSort, arr1, n);
-    SortTestHelper::testSort("Quick Sort", quickSort, arr2, n);
+//    SortTestHelper::testSort("Quick Sort", quickSort, arr2, n);
     SortTestHelper::testSort("Quick Sort 2 pointers", quickSort2, arr3, n);
+    SortTestHelper::testSort("Quick Sort 3 Ways", quickSort3Ways, arr4, n);
 
     delete[] arr1;
     delete[] arr2;
     delete[] arr3;
+    delete[] arr4;
 
     cout << endl;
 
-
+    //have a large amount of same elements
     cout << "Test for Random Array, size = " << n << ", random range [0, "<< 10 <<"]"<< endl;
     arr1 = SortTestHelper::generateRandomArray(n, 0, 10);
     arr2 = SortTestHelper::copyIntArray(arr1, n);
     arr3 = SortTestHelper::copyIntArray(arr1, n);
+    arr4 = SortTestHelper::copyIntArray(arr1, n);
 
     SortTestHelper::testSort("Merge Sort", mergeSort, arr1, n);
-    SortTestHelper::testSort("Quick Sort", quickSort, arr2, n);
-    SortTestHelper::testSort("Quick Sort 2", quickSort2, arr2, n);
+//    SortTestHelper::testSort("Quick Sort", quickSort, arr2, n);
+    SortTestHelper::testSort("Quick Sort 2", quickSort2, arr3, n);
+    SortTestHelper::testSort("Quick Sort 3 Ways", quickSort3Ways, arr4, n);
 
     delete[] arr1;
     delete[] arr2;
     delete[] arr3;
+    delete[] arr4;
 
     cout << endl;
 
